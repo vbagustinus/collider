b8ab22894a3efc7e6494f47b798e020ca21a6c97
soehadibatik <soehadibatik@gmail.com>
2026-09-20 20:02:35 +0700

    sync: normalize checkpoints after pull 2026-09-20T20:02:35

--- checkpoints/randomColliders145.js
+++ checkpoints/randomColliders145.js
@@ -1399,4 +1399,5 @@ let randomColliders145 = [
   {presentage: "99.64273601", hex: "0x45d11f1d19406dd94c6203554a09638103cb2d"},
   {presentage: "99.78838049", hex: "0x45eac14577ae6253873b9e10ccabeaaa027212"},
   {presentage: "99.81552486", hex: "0x45ef8849c068656f1a9501af579bf33b20920d"},
+  {presentage: "79.41784052", hex: "0x37e98a38f3bc7ac518228b9b293bb892d90acf"},
 ]
